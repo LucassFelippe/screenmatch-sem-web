42fdfdaa1b82aa1dc3dbbf0d4ade4d81b3c0c003
Lucas Felippe <lukzfelippe@gmail.com>
2026-02-03 21:12:10 -0400

    update

--- src/main/java/br/com/alura/screenmatch/Principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/Principal/Principal.java
@@ -1,9 +1,13 @@
 package br.com.alura.screenmatch.Principal;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporadas;
 import br.com.alura.screenmatch.service.ConsumoAPI;
 import br.com.alura.screenmatch.service.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
@@ -25,7 +29,26 @@ public class Principal {
         DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
         System.out.println(dados);
 
+        List<DadosTemporadas> temporadas = new ArrayList<>();
 
+		for(int i=1;i<dados.totalTemporadas();i++){
+			json = consumo.obterDados(ENDERECO + APIKEY + nomeSerie.replace(" ", "+") + "&season=" + i);
+			DadosTemporadas dadosTemporadas = conversor.obterDados(json, DadosTemporadas.class);
+			temporadas.add(dadosTemporadas);
+		}
+
+		temporadas.forEach(System.out::println);
+
+//        for (int i = 0; i < temporadas.size(); i++) {
+//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//
+//            for (int j = 0; j < episodiosTemporada.size(); j++) {
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//
+//            }
+//        }
+
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
 
 
 
--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -15,19 +15,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		Principal principal = new Principal();
-
 		principal.exibeMenu();
-
-
-//		List<DadosTemporadas> temporadas = new ArrayList<>();
-//
-//		for(int i=1;i<dados.totalTemporadas();i++){
-//			json = consumoApi.obterDados("http://www.omdbapi.com/?apikey=98f10c4b&i=tt0238784&Season="+ i);
-//			DadosTemporadas dadosTemporadas = conversor.obterDados(json, DadosTemporadas.class);
-//			temporadas.add(dadosTemporadas);
-//		}
-//
-//		temporadas.forEach(System.out::println);
 	}
 
 
